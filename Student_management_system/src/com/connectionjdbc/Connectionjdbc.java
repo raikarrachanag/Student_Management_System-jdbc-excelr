63b22599015d0fd606e8adedf701fead28d4cfd2
unknown <rachanagraikar>
2023-08-13 17:28:57 +0530

    Student Management System

---
+++ Student_management_system/src/com/connectionjdbc/Connectionjdbc.java
@@ -0,0 +1,37 @@
+package com.connectionjdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Connectionjdbc 
+{
+	static Connection con;
+
+	public static Connection createC()
+	{
+		
+		
+		try {
+			//load the driver class 
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			
+			//create the connection...
+			String user ="root";
+			String password="Rachana@2001";
+			String url="jdbc:mysql://localhost:3306/Student_Management_System_Application";
+			
+			
+			con= DriverManager.getConnection(url,user,password);
+			
+		} 
+		catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+		return con;
+	}
+}
